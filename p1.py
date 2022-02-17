5388814b678342f80f47032622f6afb35d315b75
email <mmb2020max@gmail.com>
2022-02-17 14:49:36 +0200

    Parctise Numpy in Python 1

---
+++ p1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# print(np.__version__)
+
+# list1=[12,23,22,23,2,45,5,6,5,4,8,565]
+# tuple1=(65,5,6,78,89,8,9,45,12)
+# arr1=np.array(list1)
+# arr2=np.array(tuple1)
+# print(list1)
+# print(tuple1)
+# print(arr1)
+# print(arr2)
+# list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
+# arr4=np.array(list2)
+# print(arr4)
+# list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
+# arr4=np.array(list3)
+# print(arr4)
+
+list1=[12,23,22,23,2,45,5,6,5,4,8,565]
+list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
+list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
+
+# print(len(list1))
+arr7=np.array(list1)
+arr8=np.array(list2)
+arr9=np.array(list3)
+print(arr7)
+print(arr8)
+print(arr9)
+
+
