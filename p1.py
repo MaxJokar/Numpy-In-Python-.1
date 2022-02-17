9d55d342dd98e65f5faa678e01c0750d1b09b2ce
email <mmb2020max@gmail.com>
2022-02-17 18:06:26 +0200

    random in Numpy

--- p1.py
+++ p1.py
@@ -196,17 +196,64 @@ import numpy as np
 # list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
 # list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
 
-arr1=np.array([12,23,22,23,2,45,5,6,5,4,8,565])
-list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
-list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
+# arr1=np.array([12,23,22,23,2,45,5,6,5,4,8,565])
+# list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
+# list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
+# arr4=np.array([2,5,6,8,9,12,23,56,89])
+
+# x=np.where(arr1%2==0)
+# x=np.where(arr1==23)
+
+# x=np.searchsorted(arr4,8)
+# x=np.searchsorted(arr4,8,side='right')
+#array must be in normal number from small to big
+# print(x)
+
+
+# arr1=np.array([12,23,22,23,2,45,5,6,5,4,8,565])
+# list2=[[1,2,3,4],[4,5,6,5],[7,8,9,78]]
+# list3=[[[1,2,3],[7,8,9],[9,8,7]],[[1,2,3],[7,8,9],[9,8,7]]]
+# arr4=np.array([2,5,6,8,9,12,23,56,89])
+
+# print(np.sort(arr1))
+
+# filter ++++++++++++++++++++++++++++++++++++++++
+
+
+# arr1=np.array([12,45,5,8,565])
+# listfilter=[True,True,False,False,True]
+# print(arr1[listfilter])
+
+
+# arr1=np.array([12,45,5,8,565])
+
+# listfilter=[]
+# for item in arr1:
+#     if item %2==0:
+#         listfilter.append(True)
+#     else:
+#         listfilter.append(False)    
+# # print(listfilter)
+# print(arr1[listfilter])
 
 
 
-x=np.where(arr1==23)
+
+# arr1=np.array([12,45,5,8,565])
+
+# listFilter=arr1 % 2==1
+# print(listFilter)
+# print(arr1[listFilter])
+
+
+# listFilter=arr1>20
+# print(arr1[listFilter])
+
+#==============================PART 4===============================
+from numpy import random
+#rand function produces random numbers between 0 and 1 , 5 times
+x=random.rand(5) 
 print(x)
-
-
-
 
 
 
